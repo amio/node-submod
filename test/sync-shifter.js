f02561990eb91650e957f81fde1876c48d5017f1
Amio <amio.cn@gmail.com>
2015-08-09 15:24:20 +0800

    (test) More tests.

--- test/sync-shifter.js
+++ test/sync-shifter.js
@@ -1,10 +1,41 @@
 const tape = require('tape')
 const shifter = require('../lib/sync-shifter')
+const fs = require('fs')
+const path = require('path')
 
 tape.test('sync-shifter', function (t) {
   //
   t.test('shift srt subtitle', function (tt) {
     tt.ok(typeof shifter === 'function')
+
+    const subtitle = fs.readFileSync(
+      path.join(__dirname, './sync-shifter.srt'),
+      { encoding: 'utf8' }
+    )
+    const expected = fs.readFileSync(
+      path.join(__dirname, './sync-shifter.expected.srt'),
+      { encoding: 'utf8' }
+    )
+
+    tt.equal(shifter(subtitle, 15123, 'srt'), expected)
+
+    tt.end()
+  })
+
+  t.test('shift ass subtitle', function (tt) {
+    tt.ok(typeof shifter === 'function')
+
+    const subtitle = fs.readFileSync(
+      path.join(__dirname, './sync-shifter.ass'),
+      { encoding: 'utf8' }
+    )
+    const expected = fs.readFileSync(
+      path.join(__dirname, './sync-shifter.expected.ass'),
+      { encoding: 'utf8' }
+    )
+
+    tt.equal(shifter(subtitle, -134900, 'ass'), expected)
+
     tt.end()
   })
 
