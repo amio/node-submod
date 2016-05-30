20244f2ee49d4b80d515ac4957f1e39fad50abb1
Amio <amio.cn@gmail.com>
2016-05-30 23:12:37 +0800

    Refine tests.

--- test/sync-shifter.js
+++ test/sync-shifter.js
@@ -4,10 +4,12 @@ const fs = require('fs')
 const path = require('path')
 
 tape.test('sync-shifter', function (t) {
-  //
-  t.test('shift srt subtitle', function (tt) {
-    tt.ok(typeof shifter === 'function')
+  t.test('is a function', function (tt) {
+    tt.ok(typeof shifter === 'function', 'Correct shifter module type.')
+    tt.end()
+  })
 
+  t.test('shift srt subtitle', function (tt) {
     const subtitle = fs.readFileSync(
       path.join(__dirname, './sync-shifter.source.srt'),
       { encoding: 'utf8' }
@@ -17,14 +19,13 @@ tape.test('sync-shifter', function (t) {
       { encoding: 'utf8' }
     )
 
-    tt.equal(shifter(subtitle, 15123, 'srt'), expected)
+    const result = shifter(subtitle, 15123, 'srt')
+    tt.equal(result, expected, 'Increase srt subtitle delay by 15.123 seconds.')
 
     tt.end()
   })
 
   t.test('shift ass subtitle', function (tt) {
-    tt.ok(typeof shifter === 'function')
-
     const subtitle = fs.readFileSync(
       path.join(__dirname, './sync-shifter.source.ass'),
       { encoding: 'utf8' }
@@ -34,7 +35,8 @@ tape.test('sync-shifter', function (t) {
       { encoding: 'utf8' }
     )
 
-    tt.equal(shifter(subtitle, -134900, 'ass'), expected)
+    const result = shifter(subtitle, -134900, 'ass')
+    tt.equal(result, expected, 'Decrease ass subtitle delay by 134.9 seconds.')
 
     tt.end()
   })
